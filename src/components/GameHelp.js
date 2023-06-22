4700a3055a9885bcab43c7de9d47d517c00caa42
99slayer <21979862+99slayer@users.noreply.github.com>
2023-06-22 17:35:46 -0600

    - Setup highscores and gamehelp components to work with the header component.

--- src/components/GameHelp.js
+++ src/components/GameHelp.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "../styles/GameHelp.css";
 
-export const GameHelp = () => {
-  return (
-    <div id="gamehelp-component">
+export const GameHelp = (props) => {
+  const { isOpen, closeFunc } = props;
 
+  return (
+    <div id="gamehelp-component" className={isOpen ? 'open' : ''}>
+      <div id="gamehelp-header">
+        <h2 id="gamehelp-heading">GAME HELP</h2>
+        <button id="gamehelp-exit-btn" onClick={() => { closeFunc('gamehelp') }}>X</button>
+      </div>
+      <div id="gamehelp-info">
+        <p>PLACEHOLDER</p>
+      </div>
     </div>
   );
 };
--- src/components/HighScores.js
+++ src/components/HighScores.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "../styles/HighScores.css";
 
-export const HighScores = () => {
-  return (
-    <div id="highscores-component">
+export const HighScores = (props) => {
+  const { isOpen, closeFunc } = props;
 
+  return (
+    <div id="highscores-component" className={isOpen ? 'open' : ''}>
+      <div id="highscore-header">
+        <h2 id="highscore-heading">HIGH SCORES</h2>
+        <button id="highscore-exit-btn" onClick={() => { closeFunc('highscores') }}>X</button>
+      </div>
+      <ul>
+        {/* highscores */}
+      </ul>
     </div>
   );
 };
