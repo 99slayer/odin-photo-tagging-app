19b47dde2901086aec62564d4ffcb7529f4613df
99slayer <21979862+99slayer@users.noreply.github.com>
2023-06-21 18:51:53 -0600

    - Created files for the highscores and gamehelp components.

---
+++ src/components/GameHelp.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../styles/GameHelp.css";
+
+export const GameHelp = () => {
+  return (
+    <div id="gamehelp-component">
+
+    </div>
+  );
+};
---
+++ src/components/HighScores.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../styles/HighScores.css";
+
+export const HighScores = () => {
+  return (
+    <div id="highscores-component">
+
+    </div>
+  );
+};
