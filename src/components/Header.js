ad35370b3cd0f28649b414fff4193ec96fb748cd
99slayer <21979862+99slayer@users.noreply.github.com>
2023-06-22 17:24:26 -0600

    - Added the highscores and gamehelp components to Header. - Added functions to open and close the highscores and gamehelp screens.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Header.css";
+import { HighScores } from "./HighScores";
+import { GameHelp } from "./GameHelp";
 
 export const Header = () => {
+  const [openHighScores, setOpenHighScores] = useState(false);
+  const [openGameHelp, setOpenGameHelp] = useState(false);
+
+  const openScreen = (btn) => {
+    switch (btn) {
+      case 'highscores':
+        if (openHighScores) {
+          break;
+        } else if (openGameHelp) {
+          setOpenGameHelp(false);
+          setOpenHighScores(true);
+          break;
+        } else {
+          setOpenHighScores(true);
+          break;
+        };
+
+      case 'gamehelp':
+        if (openGameHelp) {
+          break;
+        } else if (openHighScores) {
+          setOpenHighScores(false);
+          setOpenGameHelp(true);
+          break;
+        } else {
+          setOpenGameHelp(true);
+          break;
+        };
+
+      default:
+        console.log('No case match.');
+        break;
+    };
+  };
+
+  const closeScreen = (btn) => {
+    switch (btn) {
+      case 'highscores':
+        setOpenHighScores(false);
+        break;
+      case 'gamehelp':
+        setOpenGameHelp(false);
+        break;
+      default:
+        console.log(`ERROR: problem closing screen ${btn}.`);
+    };
+  };
+
   return (
+    // should really change these id names
     <div id="header-component">
-      <h1 id="heading">Odin Photo Search</h1>
-      <div id="header-btns">
-        <button>High Scores</button>
-        <button>Game Help</button>
+      <div id="header-cont">
+        <div id="heading-cont">
+          <h1 id="heading">Odin Photo Search</h1>
+          <div id="header-btns">
+            <button onClick={() => { openScreen('highscores') }}>High Scores</button>
+            <button onClick={() => { openScreen('gamehelp') }}>Game Help</button>
+          </div>
+        </div>
+        <div id="target-cont">
+          <p>target one</p>
+          <p>target two</p>
+          <p>target three</p>
+        </div>
+        <HighScores isOpen={openHighScores} closeFunc={closeScreen} />
+        <GameHelp isOpen={openGameHelp} closeFunc={closeScreen} />
       </div>
     </div>
   );
