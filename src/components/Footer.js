09ece2c6e117ad36d10609cd3e84c5fba2ac6458
99slayer <21979862+99slayer@users.noreply.github.com>
2023-06-22 17:23:14 -0600

    - Setup basic footer component.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,9 @@ import "../styles/Footer.css";
 
 export const Footer = () => {
   return (
-    <div></div>
+    <div id="footer-component">
+      <div id="footer-github-link"></div>
+      <div id="footer-image-credit"></div>
+    </div>
   );
 };
