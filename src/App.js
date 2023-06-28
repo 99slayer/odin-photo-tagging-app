8ddf7465866235d1295c79c32b4541f188a9904c
99slayer <21979862+99slayer@users.noreply.github.com>
2023-06-28 14:47:57 -0600

    - Made a function that watches for the browser window to change size, and will change the app components width setting accordingly.
    - Made a function that watches for the image to change size, and will
    change the app components width setting accordingly.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './styles/App.css';
 import { Header } from './components/Header';
 import { Main } from './components/Main';
 import { Footer } from './components/Footer';
 
 function App() {
+  const [changeWidth, setChangeWidth] = useState(false);
+  // setting the image min-width to 100% solved my issue
+  useEffect(() => {
+    window.onresize = () => {
+      if (document.getElementById('app-component').offsetWidth < document.querySelector('html').clientWidth) {
+        setChangeWidth(false);
+      } else if (document.getElementById('app-component').offsetWidth < document.getElementById('image').offsetWidth) {
+        setChangeWidth(true);
+      };
+      console.log('window was resized');
+    }
+  })
+
+  const checkImgWidth = (windowWidth, imageWidth) => {
+    if (imageWidth > windowWidth) {
+      setChangeWidth(true);
+    } else if (imageWidth <= windowWidth) {
+      setChangeWidth(false);
+    };
+  };
+
   return (
-    <div id='app-component'>
+    <div id='app-component' className={changeWidth ? 'app-max' : ''}>
       <Header />
-      <Main />
+      <Main checkImgWidth={checkImgWidth} />
       <Footer />
     </div>
   );
