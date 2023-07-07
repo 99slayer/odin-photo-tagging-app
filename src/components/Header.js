aea6848119ead248771017681899bc91db298ce5
99slayer <21979862+99slayer@users.noreply.github.com>
2023-07-07 16:48:35 -0600

    - Moved the highscores and gamehelp components to the gamescreen component.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,71 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import "../styles/Header.css";
-import { HighScores } from "./HighScores";
-import { GameHelp } from "./GameHelp";
 
 export const Header = () => {
-  const [openHighScores, setOpenHighScores] = useState(false);
-  const [openGameHelp, setOpenGameHelp] = useState(false);
-
-  const openScreen = (btn) => {
-    switch (btn) {
-      case 'highscores':
-        if (openHighScores) {
-          break;
-        } else if (openGameHelp) {
-          setOpenGameHelp(false);
-          setOpenHighScores(true);
-          break;
-        } else {
-          setOpenHighScores(true);
-          break;
-        };
-
-      case 'gamehelp':
-        if (openGameHelp) {
-          break;
-        } else if (openHighScores) {
-          setOpenHighScores(false);
-          setOpenGameHelp(true);
-          break;
-        } else {
-          setOpenGameHelp(true);
-          break;
-        };
-
-      default:
-        console.log('No case match.');
-        break;
-    };
-  };
-
-  const closeScreen = (btn) => {
-    switch (btn) {
-      case 'highscores':
-        setOpenHighScores(false);
-        break;
-      case 'gamehelp':
-        setOpenGameHelp(false);
-        break;
-      default:
-        console.log(`ERROR: problem closing screen ${btn}.`);
-    };
-  };
-
   return (
-    // should really change these id names
-    <div id="header-component">
-      <div id="header-cont">
-        <div id="heading-cont">
-          <h1 id="heading">Odin Photo Search</h1>
-          <div id="header-btns">
-            <button onClick={() => { openScreen('highscores') }}>High Scores</button>
-            <button onClick={() => { openScreen('gamehelp') }}>Game Help</button>
-          </div>
-        </div>
-        <HighScores isOpen={openHighScores} closeFunc={closeScreen} />
-        <GameHelp isOpen={openGameHelp} closeFunc={closeScreen} />
-      </div>
+    <div id="header-component" data-testid="header">
+      <h1 id="heading">Odin Photo Search</h1>
     </div>
   );
 };
