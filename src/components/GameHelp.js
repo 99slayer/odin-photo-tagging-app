5819ab82c2d0eca7830a32d8971ac7c1e2cb3269
99slayer <21979862+99slayer@users.noreply.github.com>
2023-07-07 16:50:31 -0600

    - Added a few different element attributes.

--- src/components/GameHelp.js
+++ src/components/GameHelp.js
@@ -2,13 +2,26 @@ import React from "react";
 import "../styles/GameHelp.css";
 
 export const GameHelp = (props) => {
-  const { isOpen, closeFunc } = props;
+  const { isOpen, closeFunc, gamescreenPosition } = props;
 
   return (
-    <div id="gamehelp-component" className={isOpen ? 'open' : ''}>
+    <div
+      id="gamehelp-component"
+      className={isOpen ? "open-flex" : ""}
+      data-testid="gamehelp-screen"
+      style={{ left: gamescreenPosition }}
+    >
       <div id="gamehelp-header">
         <h2 id="gamehelp-heading">GAME HELP</h2>
-        <button id="gamehelp-exit-btn" onClick={() => { closeFunc('gamehelp') }}>X</button>
+        <button
+          id="gamehelp-exit-btn"
+          onClick={() => {
+            closeFunc("gamehelp");
+          }}
+          data-testid="gamehelp-exit-btn"
+        >
+          X
+        </button>
       </div>
       <div id="gamehelp-info">
         <p>PLACEHOLDER</p>
--- src/components/HighScores.js
+++ src/components/HighScores.js
@@ -2,17 +2,28 @@ import React from "react";
 import "../styles/HighScores.css";
 
 export const HighScores = (props) => {
-  const { isOpen, closeFunc } = props;
+  const { isOpen, closeFunc, gamescreenPosition } = props;
 
   return (
-    <div id="highscores-component" className={isOpen ? 'open' : ''}>
+    <div
+      id="highscores-component"
+      className={isOpen ? "open-flex" : ""}
+      data-testid="highscore-screen"
+      style={{ left: gamescreenPosition }}
+    >
       <div id="highscore-header">
         <h2 id="highscore-heading">HIGH SCORES</h2>
-        <button id="highscore-exit-btn" onClick={() => { closeFunc('highscores') }}>X</button>
+        <button
+          id="highscore-exit-btn"
+          onClick={() => {
+            closeFunc("highscores");
+          }}
+          data-testid="highscore-exit-btn"
+        >
+          X
+        </button>
       </div>
-      <ul>
-        {/* highscores */}
-      </ul>
+      <ul>{/* highscores */}</ul>
     </div>
   );
 };
