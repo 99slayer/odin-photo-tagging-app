5bdd821c09de4f439b8bdf1f02217387e0520c75
99slayer <21979862+99slayer@users.noreply.github.com>
2023-07-10 14:15:48 -0600

    - Created a file to hold functions related to coordinates, and working with coordinates.

---
+++ src/coordinates.js
@@ -0,0 +1,38 @@
+export const coordinates = {
+  validate(targetRange, coordinates) {
+    let isValid = {
+      x: null,
+      y: null,
+    };
+
+    if (
+      coordinates.x >= targetRange.x.low &&
+      coordinates.x <= targetRange.x.high
+    ) {
+      isValid.x = true;
+    }
+
+    if (
+      coordinates.y >= targetRange.y.low &&
+      coordinates.y <= targetRange.y.high
+    ) {
+      isValid.y = true;
+    }
+
+    if (isValid.x && isValid.y) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+  convert(coordinates, currentImageSize) {
+    const defaultWidth = 3030;
+    const defaultHeight = 1952;
+    const convertedCoordinates = {
+      x: Math.round(defaultWidth * (coordinates.x / currentImageSize.width)),
+      y: Math.round(defaultHeight * (coordinates.y / currentImageSize.height)),
+    };
+
+    return convertedCoordinates;
+  },
+};
