def4db26b72798757da09a9fa745fc3aefa9f591
99slayer <21979862+99slayer@users.noreply.github.com>
2023-07-10 14:17:24 -0600

    - Created a file to hold the different target positions. Keeping any reference to a targets position consistent across the project.

---
+++ src/targetPositions.js
@@ -0,0 +1,32 @@
+export const targetPositions = {
+  chameleon: {
+    x: {
+      low: 1414,
+      high: 1455,
+    },
+    y: {
+      low: 1908,
+      high: 1967,
+    },
+  },
+  donkey: {
+    x: {
+      low: 1300,
+      high: 1376,
+    },
+    y: {
+      low: 1058,
+      high: 1151,
+    },
+  },
+  cat: {
+    x: {
+      low: 1439,
+      high: 1492,
+    },
+    y: {
+      low: 999,
+      high: 1065,
+    },
+  },
+};
