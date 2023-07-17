20373a6e20eb329d5503d746e63a1dce2eb30660
99slayer <21979862+99slayer@users.noreply.github.com>
2023-07-17 17:17:49 -0600

    - The Start component now houses it's own DOM logic.

--- src/components/Start.js
+++ src/components/Start.js
@@ -1,20 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/Start.css";
 
 export const Start = (props) => {
-  const { gameStart, setGameStart } = props;
+  const { setGameStart } = props;
+
+  // Moved Start components own logic into itself <= START HERE
+  const [open, setOpen] = useState(true);
+
+  useEffect(() => {
+    setGameStart(!open);
+  }, [open]);
 
   return (
     <div
       id="start-cont"
-      className={gameStart ? "closed" : "open-flex"}
+      className={open ? "open-flex" : "closed"}
       data-testid="start-cont"
     >
       <p id="start-text">Try and find the targets as fast as you can!</p>
       <button
         id="start-btn"
         onClick={() => {
-          setGameStart(true);
+          setOpen(false);
         }}
       >
         START
