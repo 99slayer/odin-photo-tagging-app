8f2a07f84af2b262435defdee1914feb58326089
99slayer <21979862+99slayer@users.noreply.github.com>
2023-07-19 15:09:50 -0600

    - Added a test for coordinates.convert.

--- src/__tests__/coordinates.test.js
+++ src/__tests__/coordinates.test.js
@@ -3,7 +3,7 @@ import { targetPositions } from "../targetPositions";
 
 describe("coordinates", () => {
   describe("validate", () => {
-    it("returns false when passed invalid coordinates", () => {
+    it("Returns false when passed invalid coordinates.", () => {
       const coords1 = {
         x: 1213,
         y: 1500,
@@ -22,15 +22,15 @@ describe("coordinates", () => {
       ).toBeFalsy();
     });
 
-    it("returns true when passed valid coordinates", () => {
+    it("Returns true when passed valid coordinates.", () => {
       const coords1 = {
         x: 1430,
-        y: 1930,
+        y: 1800,
       };
 
       const coords2 = {
-        x: 1455,
-        y: 1967,
+        x: 1410,
+        y: 1820,
       };
 
       expect(
@@ -39,6 +39,28 @@ describe("coordinates", () => {
       expect(
         coordinates.validate(targetPositions.chameleon, coords2)
       ).toBeTruthy();
+    });
+  });
+
+  describe("convert.", () => {
+    const coords = {
+      x: 514,
+      y: 396,
+    };
+
+    const currentImgSize = {
+      width: 1068,
+      height: 688,
+    };
+
+    it("Correctly converts a given set of coordinates.", () => {
+      const convertedSet = {
+        x: coordinates.convert(coords, currentImgSize).x,
+        y: coordinates.convert(coords, currentImgSize).y,
+      };
+
+      expect(convertedSet.x).toEqual(1458);
+      expect(convertedSet.y).toEqual(868);
     });
   });
 });
