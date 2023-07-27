db69c3fae9a3274834b97cc416c219e527701cfa
99slayer <21979862+99slayer@users.noreply.github.com>
2023-07-27 14:20:30 -0600

    - Created the getHighScores function which pulls highscore data from the backend, puts it into and array, and returns it as a promise.
    - Created the addHighScore function which saves the users time and
    name to the backend.

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,6 +1,13 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore, doc, getDoc } from "firebase/firestore";
+import {
+  getFirestore,
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  setDoc,
+} from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCmoRXHa8A2lNwVLiDkIdO7oiCuFmpGujY",
@@ -8,7 +15,7 @@ const firebaseConfig = {
   projectId: "odin-photo-tagging-app-99",
   storageBucket: "odin-photo-tagging-app-99.appspot.com",
   messagingSenderId: "1060505103507",
-  appId: "1:1060505103507:web:5e5534f4ad9070e8da7166"
+  appId: "1:1060505103507:web:5e5534f4ad9070e8da7166",
 };
 
 const app = initializeApp(firebaseConfig);
@@ -22,5 +29,34 @@ export async function getTarget(target) {
     return docSnap.data();
   } else {
     console.log("No such document!");
-  };
+  }
+}
+
+export const getHighScores = async () => {
+  const querySnapshot = await getDocs(collection(db, "HighScores"));
+  const highScores = [];
+
+  querySnapshot.forEach((doc) => {
+    const data = doc.data();
+    highScores.push({
+      time: data.time,
+      name: data.name,
+    });
+  });
+
+  return highScores;
+};
+
+// SANITIZE INPUT !!
+export const addHighScore = async (name, time) => {
+  // time should be saved in milliseconds
+  const numberOfScores = await getHighScores().then((value) => {
+    return Object.keys(value).length;
+  });
+
+  // IN THE FUTURE ADD A TIME-STAMP FIELD
+  await setDoc(doc(db, "HighScores", `${numberOfScores + 1}`), {
+    name,
+    time,
+  });
 };
